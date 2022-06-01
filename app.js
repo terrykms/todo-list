0024c1a44ca940eb59f46697fca727a202c5255b
Kim Minseo <terrykms30@gmail.com>
2022-06-01 12:16:02 +0800

    updated localStorage function

--- app.js
+++ app.js
@@ -1,60 +1,53 @@
-let tasks = [];
-window.onload = retrieveFromLocalStorage;
-function addToLocalStorage(tasks) {
-    localStorage.setItem("tasks", JSON.stringify(tasks))
+let tasks = []; // create an empty array of tasks 
 
+window.onload = function() {
+    tasks = retrieveFromLocalStorage();
+    renderTasks(tasks);
 }
 
+/* ============= LOCAL STORAGE FUNCTIONS ============== */
+function updateLocalStorage(tasks) {
+    localStorage.setItem("tasks", JSON.stringify(tasks))
+}
+
+// returns "tasks" JSON Object 
 function retrieveFromLocalStorage() {
     const reference = localStorage.getItem("tasks");
     if (reference) {
-        tasks = JSON.parse(reference);
-        renderTasks(tasks);
+        return JSON.parse(reference);
     }
 }
 
+// used when the page reloads - iterates through every task saved in local storage as JSON
+// function renderTask() is defined below
+function renderTasks(tasks) {
+    tasks.forEach(renderTask)
+}
 
-//for debugging (clear task)
-document.getElementById("clrLocalStorage").addEventListener("click", function() {
-    localStorage.clear();
-})
-
-
-//define functions
-
-function createButton(className) {
-    var button = document.createElement("span");
-    button.setAttribute("class", className)
-    textNode = document.createTextNode("\u2573");
-    button.appendChild(textNode);
-
-    // create function to remove task when clicked
-    button.addEventListener("click", function() {
-        var data_id = this.parentNode.id
-        this.parentNode.remove();
-
-        // NOT WORKING
-        const reference = localStorage.getItem("tasks");
-        tasks = JSON.parse(reference)
-        tasks.filter(function(item) {
-            key = document.getElementById(data_id);
-            return key != item.id
-        })
-
-    })
-
-    return button;
+// remove an item from localstorage
+function removeTaskFromLocalStorage(taskID) {
+    tasks = retrieveFromLocalStorage()
+    updatedTasks = tasks.filter(item => item.id != taskID)
+    updateLocalStorage(updatedTasks);
 }
 
 
-// adding task, uses two other functions: saveTaskAsJSON() and renderTask(). 
+
+/* ==================================
+    function addTask(): 
+        uses  other functions: 
+            - saveTaskAsJSON()
+            - updateLocalStorage()
+            - renderTask()
+======================================= */
+
 function addTask() {
     var taskValue = document.getElementById("myTask").value;
 
     if (taskValue != "") {
         // JSON
         taskObject = saveTaskAsJSON(taskValue, tasks);
-        addToLocalStorage(tasks);
+        updateLocalStorage(tasks);
 
         // Add task into HTML 
         renderTask(taskObject);
@@ -64,9 +57,17 @@ function addTask() {
     }
 }
 
-// used when the page reloads - iterates through every task saved in local storage as JSON
-function renderTasks(tasks) {
-    tasks.forEach(renderTask)
+function saveTaskAsJSON(taskValue, tasks) {
+    // JSON
+    const taskObject = {
+        id: Date.now(),
+        value: taskValue,
+        checked: false
+    }
+
+    tasks.push(taskObject);
+
+    return taskObject
 }
 
 
@@ -88,28 +89,52 @@ function renderTask(taskObject) {
     taskList.appendChild(li);
 }
 
-function saveTaskAsJSON(taskValue, tasks) {
-    // JSON
-    const taskObject = {
-        id: Date.now(),
-        value: taskValue,
-        checked: false
-    }
 
-    tasks.push(taskObject);
+function createButton(className) {
+    var button = document.createElement("span");
+    button.setAttribute("class", className)
+    textNode = document.createTextNode("\u2573");
+    button.appendChild(textNode);
 
-    return taskObject
+    // create function to remove task when clicked
+    button.addEventListener("click", function() {
+        this.parentNode.remove();
+        
+        var data_id = this.parentNode.id;
+        removeTaskFromLocalStorage(data_id)
+
+    })
+
+    return button;
 }
 
 
 
-// Event Handlers
+/* ================ EVENT HANDLERS ===================== */
+
 var ul = document.querySelector("ul");
 ul.addEventListener("click", function(element) {
     if (element.target.tagName == "LI" ) {
         element.target.classList.toggle("checked");
+
+        // ADD FUNCTION TO UPDATE "CHECKED" BOOLEAN VALUE IN JSON OBJECT
+        tasks = retrieveFromLocalStorage();
     }
 })
 
 var addButton = document.getElementById("submitTask");
 addButton.addEventListener("click", addTask);
+
+// clear task in HTML DOM and localStorage
+document.getElementById("clrLocalStorage").addEventListener("click", function() {
+    // clear screen 
+    currentTasks = document.querySelectorAll(".item");
+    console.log(currentTasks);
+
+    currentTasks.forEach(function(item) {
+        item.remove();
+    })
+
+    // clear data in localStorage
+    localStorage.clear();
+})
